921176244dc8f154f39cceaaf3557c951f226173
Ievgenii Strygul <strygul@gmail.com>
2026-02-21 23:26:45 +0100

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://strygul.com',
-	base: '/strygul-blog/',
+	base: '/blog/',
 	integrations: [mdx(), sitemap()],
 });
