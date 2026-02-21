75b76fe68e2b1c2710b7bbbba7a11cf4aff90a3e
Ievgenii Strygul <strygul@gmail.com>
2026-02-21 23:44:11 +0100

    Astro config changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://strygul.com',
-	base: '/blog/',
+	base: '/',
 	integrations: [mdx(), sitemap()],
 });
